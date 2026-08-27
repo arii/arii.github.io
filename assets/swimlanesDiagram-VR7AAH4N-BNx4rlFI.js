e8e1f96d5e4235e4981a8dfcd2c2050a734cd3b4
arii <anders.ariel@gmail.com>
2026-08-27 18:02:40 +0000

    Deploying to main from @ arii/portfolio@7630a68784b4558dc631ddd482f8f21c585f3856 🚀

---
+++ assets/swimlanesDiagram-VR7AAH4N-BNx4rlFI.js
@@ -0,0 +1,8 @@
+import{c as e,s as r}from"./flowDiagram-HODETNUW-DZBpRRl8.js";import{_ as a}from"./index-CJEWw8Nq.js";import"./chunk-5VM5RSS4-CaE0dxWy.js";import"./chunk-XXDRQBXY-Dk_2ZT2n.js";import"./chunk-POPQ4Y6H-DD9pNz1I.js";import"./chunk-F27PBJKO-C5TNMypw.js";import"./channel-DsQvNvx6.js";var o=a(t=>`${r(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),s=o,f=e({defaultLayout:"swimlane",styles:s});export{f as diagram};
